46c675415f9a4cc84df12dfcf93f01694fdbc0e4
novinary <novina.dev@gmail.com>
2019-02-21 14:10:56 +0000

    Used fontawesome for social media icons

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,15 +1,22 @@
 import React from 'react'
 
+const SearchBar= (props) => {
+    return(
+        <div className="search-bar">
+            <div className="left-side">
+            <i className="fab fa-instagram"></i>
+            <hr/>
+            <h2 className="instagram-logo">Instagram</h2>
+            </div>
+            <input className = "seach-input" type="text" placeholder= "Search"/>
+            <div className="search-icons">
+            <i class="far fa-compass"></i>
+            <i class="far fa-heart"></i>
+            <i class="far fa-user"></i>
+            </div>
 
-
-
-
-
-
-
-
-
-
-
+        </div>
+    )
+}
 
 export default SearchBar
