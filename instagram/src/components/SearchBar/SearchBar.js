beccdc0ca708a0e2b213c6ed5bde0b7e58f5318c
novinary <novina.dev@gmail.com>
2019-02-21 15:13:37 +0000

    SearchBar looking good

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -14,14 +14,14 @@ const SearchBar= (props) => {
         <div className="search-bar">
             <div className="left-side">
             <i className="fab fa-instagram"></i>
-            <hr/>
+            <h1>|</h1>
             <h2 className="instagram-logo">Instagram</h2>
             </div>
             <input className = "seach-input" type="text" placeholder= "Search"/>
             <div className="search-icons">
-            <i class="far fa-compass"></i>
-            <i class="far fa-heart"></i>
-            <i class="far fa-user"></i>
+            <i className="far fa-compass"></i>
+            <i className="far fa-heart"></i>
+            <i className="far fa-user"></i>
             </div>
 
         </div>
