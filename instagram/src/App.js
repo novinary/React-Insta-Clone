8962a57edd92f60b00e46d27ab8e4e145608fe1a
novinary <novina.dev@gmail.com>
2019-02-22 07:13:01 +0000

    MVP for day 1 looking good

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar'
-
+import PostContainer from "./components/PostContainer/PostContainer";
+import dummyData from "./dummy-data";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
   render() {
     return (
       <div className="App">
-       <SearchBar />
-
-  
+        <SearchBar />
+        <PostContainer posts={dummyData} />
       </div>
     );
   }
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,9 +1,9 @@
 import React from "react";
+import Posts from "../Posts/Posts";
 
 const PostContainer = props => {
-    return (
-    
-    );
-  };
-  
+  let arr = props.posts.map((e, i) => <Posts key={i} post={e} />);
+  return <div className="posts">{arr}</div>;
+};
+
 export default PostContainer;
